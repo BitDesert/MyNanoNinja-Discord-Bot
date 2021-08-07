5714eb9c8c8d6ee6af078bf46a12821d439fba57
BitDesert <11021604+BitDesert@users.noreply.github.com>
2021-08-07 14:55:09 +0200

    commands switched

--- commands/invite.js
+++ commands/invite.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'invite',
   description: 'Displays the invite link for this bot',
   async execute(interaction) {
-    await interaction.reply('Join the TNC Discord server at https://bit.ly/nanocenter-discord');
+    await interaction.reply('https://discordapp.com/oauth2/authorize?client_id=' + interaction.client.user.id + '&scope=applications.commands');
   },
 };
--- commands/tnc.js
+++ commands/tnc.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'tnc',
   description: 'Invite for the The Nano Center Discord server',
   async execute(interaction) {
-    await interaction.reply('https://discordapp.com/oauth2/authorize?client_id=' + interaction.client.user.id + '&scope=applications.commands');
+    await interaction.reply('Join the TNC Discord server at https://bit.ly/nanocenter-discord');
   },
 };
